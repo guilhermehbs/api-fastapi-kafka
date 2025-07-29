ab05de5c8c7f37de452ed1947c76cdd1fa517079
guilhermehbs <guilhermehbs100@gmail.com>
2025-07-28 22:09:39 -0300

    config: add project api

---
+++ app/logger_config.py
@@ -0,0 +1,10 @@
+import logging
+
+def get_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s'))
+        logger.addHandler(handler)
+    return logger
